6e29d7f02d904cac2955e38b7405f94c9e764f6e
cj.green <cj.green@intimetec.com>
2022-04-07 16:44:30 -0600

    corrected ID issues

--- ContosoUniversity/Models/CourseViewModel.cs
+++ ContosoUniversity/Models/CourseViewModel.cs
@@ -3,8 +3,8 @@
     public class CourseViewModel
     {
         public int CourseID { get; set; }
-        public string Title { get; set; }
+        public string? Title { get; set; }
         public int Credits { get; set; }  
-        public string DepartmentName { get; set; }
+        public string? DepartmentName { get; set; }
     }
 }
--- ContosoUniversity/Models/StudentVM.cs
+++ ContosoUniversity/Models/StudentVM.cs
@@ -2,9 +2,9 @@
 {
     public class StudentVM
     {
-        public int Id { get; set; }
-        public string LastName { get; set; }
-        public string FirstMidName { get; set; }
+        public int ID { get; set; }
+        public string? LastName { get; set; }
+        public string? FirstMidName { get; set; }
         public DateTime EnrollmentDate { get; set; }
 
     }
